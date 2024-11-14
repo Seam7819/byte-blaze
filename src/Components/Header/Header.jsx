39771bdda9dbf64e7696e847d932bdb8b7a80a6f
Snu Seam <siamahme766@gmail.com>
2024-11-14 10:17:38 +0600

    commit project setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import {  NavLink } from "react-router-dom";
+
+const Header = () => {
+
+    const link = <>
+    <li><NavLink to='/'>Home</NavLink></li>
+    <li><NavLink to='/blog'>Blogs</NavLink></li>
+    <li><NavLink to='/bookmark'>Bookmarks</NavLink></li>
+
+    </>
+
+    const [theme, setTheme] = useState('light');
+
+    const handleToggleChange = e =>{
+        if(e.target.checked){
+            setTheme('dim')
+        }else{
+            setTheme('light')
+        }
+    }
+
+    useEffect(()=> {
+        localStorage.setItem('theme',theme)
+        const localTheme = localStorage.getItem('theme');
+        document.querySelector('html').setAttribute('data-theme', localTheme)
+    },[theme])
+
+    return (
+        <div className="navbar bg-base-100 shadow-xl fixed z-10">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {link}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl gap-0 font-bold"><span className="text-amber-400">Byte</span><span>Blaze</span></a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {link}
+                </ul>
+            </div>
+
+            {/* theme controller */}
+
+            <div className="navbar-end">
+                <label className="grid cursor-pointer place-items-center">
+                    <input
+                    onChange={handleToggleChange}
+                        type="checkbox"
+                        className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1" />
+                    <svg
+                        className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="14"
+                        height="14"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <circle cx="12" cy="12" r="5" />
+                        <path
+                            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                    </svg>
+                    <svg
+                        className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="14"
+                        height="14"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    </svg>
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
